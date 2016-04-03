93a085aee71b53d221fc09cfc8dd34301420be69
Shane <shane8641@live.com>
2016-04-02 20:29:55 -0400

    working out issues related to value not showing

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+document.getElementById("submit").addEventListener("click", titleCase, false);
+document.getElementById("result").value = titleCase();
+function titleCase() {
+    var str = document.getElementById("uString").value;
+    var arrayOfStrings = str.split(" ");
+    for(var i = 0; i < arrayOfStrings.length; i++){
+        var placeholder = arrayOfStrings[i];
+        var upLetter = placeholder.charAt[0].toUpperCase();
+        placeholder = placeholder.slice(1,placeholder.length).toLowerCase();
+        arrayOfStrings[i] = upLetter.concat(placeholder);
+    }
+    str = arrayOfStrings.join(' ');
+    return str;
+    
+}
+
 
